2316a6f64c9b43e51aa95e8ba899bceb992a66ab
Josh Price <josh@alembic.com.au>
2023-05-24 12:10:19 +1000

    wip trying to add mermaid diagrams

--- lib/ash_admin/components/resource/info.ex
+++ lib/ash_admin/components/resource/info.ex
@@ -13,6 +13,15 @@ defmodule AshAdmin.Components.Resource.Info do
     <div class="relative mx-12">
       <AttributeTable resource={@resource} />
       <RelationshipTable api={@api} resource={@resource} prefix={@prefix} />
+
+      <section phx-hook="Mermaid">
+        <h1 class="text-3xl rounded-t py-8">
+          Entity Relationship Diagram (Mermaid)
+        </h1>
+        <pre class="mermaid">
+          {Ash.Api.Info.Diagram.mermaid_er_diagram(@api)}
+        </pre>
+      </section>
     </div>
     """
   end
